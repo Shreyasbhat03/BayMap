f0e16b6ccc19ca580e8da5c33d54c39514d8f790
abhinavas-2005 <abhinavarunkumarshetty@gmail.com>
2025-09-26 18:28:32 +0530

    Updated home page

--- web-frontend/web-frontend/src/main/kotlin/presentation/App.kt
+++ web-frontend/web-frontend/src/main/kotlin/presentation/App.kt
@@ -1,17 +1,16 @@
-package presentation
-
 import react.FC
 import react.dom.html.ReactHTML.div
 import web.cssom.ClassName
 import presentation.pages.EmailVerificationPage
 import presentation.pages.HomePage
 
+
 val App = FC {
     console.log("App component is rendering") // Debug line
-
-    // Keep EmailVerificationPage
+//    div {
+//        className = ClassName("bg-blue-900 text-green p-4 rounded")
+//        +"Hello World with Kotlin React Wrap"
+//    }
     EmailVerificationPage()
-
-    // Add HomePage for testing UI
     HomePage()
 }
